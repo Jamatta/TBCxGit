30738bab0c99e9c15cf76a90dff0b5349a878075
Jamaტa <jamata94@gmail.com>
2023-10-26 04:08:33 +0400

    კლასი მოვნიშნე final - ად

--- UI/AutoLayout/AutoLayout/ViewController.swift
+++ UI/AutoLayout/AutoLayout/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet private weak var mainStackView: UIStackView!
     @IBOutlet private weak var header: UIStackView!
